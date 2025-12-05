7980f3e47c1d7d07570813785b27ecd0de7ee6d2
xceifeiro <gomesvictor46@gmail.com>
2025-12-05 11:12:28 -0300

    Header Import

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,9 +1,11 @@
-import type React from "react";
-import { AOSInit } from "@/components/AOSInit";
+import type React from "react"
+import { AOSInit } from "@/components/AOSInit"
+import { GlassmorphismNav } from "@/components/header"
 
 export default function PublicLayout({ children }: { children: React.ReactNode }) {
   return (
     <div>
+      <GlassmorphismNav />
       <AOSInit />
       {children}
     </div>
